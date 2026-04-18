14047d7c6be662afd60c97ef1d94c758305de665
Darren Jensen <darren.jensen@gmail.com>
2026-04-18 10:09:21 +0700

    update to lambda, s3, api gateway and cloudfront

--- frontend/components/twin.tsx
+++ frontend/components/twin.tsx
@@ -40,7 +40,7 @@ export default function Twin() {
         setIsLoading(true);
 
         try {
-            const response = await fetch('http://localhost:8000/chat', {
+            const response = await fetch('https://3j4521g2me.execute-api.ap-southeast-1.amazonaws.com/chat', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
